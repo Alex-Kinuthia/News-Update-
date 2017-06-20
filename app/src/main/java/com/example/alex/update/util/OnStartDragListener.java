7a737701a21a54e1a43daedc8f16ee96fa4cbff7
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-06-20 16:33:59 +0300

    new files

---
+++ app/src/main/java/com/example/alex/update/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package com.example.alex.update.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by alex on 6/20/17.
+ */
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
